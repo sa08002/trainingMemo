a27f746e2050df208b116d94cefdd835ec6f1cfa
大西幹則 <1078sa08002@gmail.com>
2021-03-07 12:40:27 +0900

    TrainingMemoDAO.jspの作成

---
+++ src/dao/TrainingMemoDAO.java
@@ -0,0 +1,46 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.TrainingMemo;
+
+public class TrainingMemoDAO {
+
+	private final String JDBC_URL =
+			"jdbc:h2:tcp://localhost/~/trainingMemo";
+//	private final String DB_USER = "sa";
+//	private final String DB_PASS = "";
+
+	public List<TrainingMemo> findAll() {
+		List<TrainingMemo> empTrainingMemo = new ArrayList<>();
+
+		try (Connection conn = DriverManager.getConnection(
+				JDBC_URL)) {
+
+			String sql = "SELECT BENCH, DEADLIFT, SQUAT FROM TRAININGMEMO";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			ResultSet rs = pStmt.executeQuery();
+
+			while (rs.next()) {
+				Double bench = rs.getDouble("BENCH");
+				Double deadlift = rs.getDouble("DEADLIFT");
+				Double squat = rs.getDouble("SQUAT");
+
+				TrainingMemo triningMemo = new TrainingMemo(bench, deadlift,squat);
+				empTrainingMemo.add(triningMemo);
+
+			}
+		} catch (SQLException e) {
+				e.printStackTrace();
+				return null;
+		}
+		return empTrainingMemo;
+	}
+}
